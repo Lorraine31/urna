8aa33ad901c19d703cb55db595562ffd27eeac67
Lorraine Letiele <lorraine.pires@unesp.com>
2021-10-03 14:19:30 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '12123',
+                nome: 'Jhon',
+                partido: 'PCM',
+                fotos: [
+                    {url:'99a.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '21214',
+                nome: 'Christine',
+                partido: 'PSM',
+                fotos: [
+                    {url: '99b.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'Alicia',
+                partido: 'PCM',
+                vice: 'Angela',
+                fotos: [
+                    {url: '98a.jpg', legenda: 'Prefeito'},
+                    {url: '98b.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '21',
+                nome: 'Robert',
+                partido: 'PSM',
+                vice: 'Alan',
+                fotos: [
+                    {url: '12345.jpg', legenda: 'Prefeito'},
+                    {url: '23456.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+    
+]
